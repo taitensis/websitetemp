01f0c57441096f13405d66a28d3561da28ed20a7
Maxime Coïc <maxime@MacBook-Air.local>
2025-09-20 23:06:48 +0200

    configure astro base + pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://<username>.github.io",
+  site: "https://taitensis.github.io",
   base: "/nourriture-quotidienne",
 
   integrations: [react(), mdx()],
